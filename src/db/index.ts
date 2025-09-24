cf93fc191d81e48e55078ff612d515110d44f319
igorpilot <ihorzhurba.air@gmail.com>
2025-09-24 15:22:41 +0200

    feat: create UserModel

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,36 +1,43 @@
-import fs from 'fs'
-import { Sequelize } from 'sequelize'
+import fs from "fs";
+import { Sequelize } from "sequelize";
+import defineExercise from "./exercise";
+import defineProgram from "./program";
+import defineUser from "./user";
+import dotenv from "dotenv";
 
-import defineExercise from './exercise'
-import defineProgram from './program'
+dotenv.config();
 
-const sequelize: Sequelize = new Sequelize('postgresql://localhost:5432/fitness_app', {
-	logging: false
-})
+const sequelize: Sequelize = new Sequelize(process.env.DB_URL, {
+  logging: false,
+});
 
-sequelize.authenticate().catch((e: any) => console.error(`Unable to connect to the database${e}.`))
+sequelize
+  .authenticate()
+  .catch((e: any) => console.error(`Unable to connect to the database${e}.`));
 
-const Exercise = defineExercise(sequelize, 'exercise')
-const Program = defineProgram(sequelize, 'program')
+const Exercise = defineExercise(sequelize, "exercise");
+const Program = defineProgram(sequelize, "program");
+const User = defineUser(sequelize, "user");
 
 const models = {
-	Exercise,
-	Program
-}
-type Models = typeof models
+  Exercise,
+  Program,
+  User,
+};
+type Models = typeof models;
 
 // check if every model is imported
-const modelsFiles = fs.readdirSync(__dirname)
+const modelsFiles = fs.readdirSync(__dirname);
 // -1 because index.ts can not be counted
-if (Object.keys(models).length !== (modelsFiles.length - 1)) {
-	throw new Error('You probably forgot import database model!')
+if (Object.keys(models).length !== modelsFiles.length - 1) {
+  throw new Error("You probably forgot import database model!");
 }
 
 Object.values(models).forEach((value: any) => {
-	if (value.associate) {
-		value.associate(models)
-	}
-})
+  if (value.associate) {
+    value.associate(models);
+  }
+});
 
-export { models, sequelize }
-export type { Models }
+export { models, sequelize };
+export type { Models };
---
+++ src/db/user.ts
@@ -0,0 +1,71 @@
+import { DataTypes, Model, Sequelize } from "sequelize";
+import { USER_ROLE } from "../utils/enums";
+
+export interface UserModel extends Model {
+  id: number;
+  name?: string;
+  surname?: string;
+  nickName?: string;
+  age?: number;
+  email: string;
+  role: USER_ROLE;
+  password: string;
+}
+
+export default (sequelize: Sequelize, modelName: string) => {
+  const UserModelCtor = sequelize.define<UserModel>(
+    modelName,
+    {
+      id: {
+        type: DataTypes.BIGINT,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      surname: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      nickName: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        unique: true,
+      },
+      age: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+          notEmpty: true,
+        },
+      },
+      role: {
+        type: DataTypes.ENUM(...Object.values(USER_ROLE)),
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+        },
+      },
+    },
+    {
+      paranoid: true,
+      timestamps: true,
+      tableName: "users",
+    }
+  );
+
+  return UserModelCtor;
+};
